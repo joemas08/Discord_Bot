e1ef4af102907a38a0af4c1cc5fe4e906f26ae68
Joseph Masone <joemasone050897@gmail.com>
2022-02-26 16:41:30 -0500

    Welcome Message with Photo

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -15,6 +18,16 @@ client.on("ready", () => {
 client.on("messageCreate", (message)=>{
     if(message.content == "hi")
         message.reply("What it dooooo")
+})
+
+const welcomeChannelId = "946284083166724116"
+
+client.on("guildMemberAdd", async (member)=>{
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content : `<@${member.id} Welcome to the server!)`,
+        files : [img]
+    })
 })
 
 client.login(process.env.TOKEN)
